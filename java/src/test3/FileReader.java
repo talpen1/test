252144f80f1b5eac4c66c48e0b3f995c56305acb
talpen1 <1600345947@qq.com>
2022-09-17 22:48:37 +0800

    关于如何撤销的改动 查看效果

--- java/src/test3/FileReader.java
+++ java/src/test3/FileReader.java
@@ -1,19 +1,19 @@
-//package test3;
-//import java.io.*;
-//
-///**
-// * 加油啊，我和程序有一个能跑就行！
-// *
-// * @author Talpen
-// * @date 2022/08/20 21:48
-// **/
-//public class FileReader {
-//    public static void main(String[] args){
-//    try{
-//        File myFile=new File("run.txt");
-//        FileReader fileReader=new FileReader(myFile);
-//        BufferedReader reader=new BufferedReader(fileReader);
-//
-//    }
-//    }
-//}
+package test3;
+import java.io.*;
+
+/**
+ * 加油啊，我和程序有一个能跑就行！
+ *
+ * @author Talpen
+ * @date 2022/08/20 21:48
+ **/
+public class FileReader {
+    public static void main(String[] args){
+    try{
+        File myFile=new File("run.txt");
+        FileReader fileReader=new FileReader(myFile);
+        BufferedReader reader=new BufferedReader(fileReader);
+
+    }
+    }
+}
