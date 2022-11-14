753318067fb828c7110e8583461478486bcb01ca
talpen1 <1600345947@qq.com>
2022-11-14 11:53:33 +0800

    去南京之前上传

---
+++ java/src/leetcode/Solution.java
@@ -0,0 +1,22 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+/**
+ * 加油啊，我和程序有一个能跑就行！
+ *
+ * @author Talpen
+ * @date 2022/11/13 20:29
+ **/
+public class Solution {
+    public List<List<Integer>> permute(int[] nums){
+        int len =nums.length;
+        List<List<Integer>> res =new ArrayList<>();
+        Collection
+
+
+    }
+
+}
--- java/src/test3/jukebox1.java
+++ java/src/test3/jukebox1.java
@@ -45,4 +45,5 @@ public class jukebox1 {
     }
 
 
+
 }
